e70bbbc5381d90a8baf9ec863de9cc7dd2acdf35
Gomez0015 <56361339+Gomez0015@users.noreply.github.com>
2019-12-21 08:11:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var downWon;
 var middleWon;
 var upWon;
 
-function startRolling(){
+function startRolling() {
   // Roll the Dice
-  function playGame(){
+  function playGame() {
     var randomNumber1 = Dice1Roll();
     var randomNumber2 = Dice2Roll();
     var randomNumber3 = Dice3Roll();
@@ -18,18 +18,18 @@ function startRolling(){
   }
 
   // Roll Dice 1
-  function Dice1Roll(){
+  function Dice1Roll() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
     var randomDice1 = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
     document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
 
-      return randomNumber1;
+    return randomNumber1;
   }
 
   // Roll Dice  2
-  function Dice2Roll(){
+  function Dice2Roll() {
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
     var randomDice2 = "images/dice" + randomNumber2 + ".png";
@@ -40,7 +40,7 @@ function startRolling(){
   }
 
   // Roll Dice  3
-  function Dice3Roll(){
+  function Dice3Roll() {
     var randomNumber3 = Math.floor(Math.random() * 6) + 1;
 
     var randomDice3 = "images/dice" + randomNumber3 + ".png";
@@ -51,22 +51,22 @@ function startRolling(){
   }
 
   // Display Which number was rolled And decide what condition wins
-  function decideWinner(randomNumber1, randomNumber2, randomNumber3){
+  function decideWinner(randomNumber1, randomNumber2, randomNumber3) {
     var numberRolled = randomNumber1 + randomNumber2 + randomNumber3;
     document.querySelector(".number-rolled").innerHTML = "Number Rolled is " + numberRolled;
     console.log(numberRolled);
 
-    if (numberRolled < 12 && numberRolled > 0){
+    if (numberRolled < 12 && numberRolled > 0) {
       downWon = true;
       middleWon = false;
       upWon = false;
     }
-    if (numberRolled == 12 ){
+    if (numberRolled == 12) {
       middleWon = true;
       downWon = false;
       upWon = false;
     }
-    if (numberRolled < 25 && numberRolled > 12){
+    if (numberRolled < 25 && numberRolled > 12) {
       upWon = true;
       middleWon = false;
       downWon = false;
@@ -79,27 +79,28 @@ function startRolling(){
   //Countdown from 15 to 0 and restart.
   startTimer();
   var tickSound = new Audio("sounds/Tick.mp3");
-  function startTimer(){
+
+  function startTimer() {
     var timeleft = 10;
-    var downloadTimer = setInterval(function(){
-      document.querySelector(".timer").innerHTML = "Rolling in "+ timeleft;
+    var downloadTimer = setInterval(function() {
+      document.querySelector(".timer").innerHTML = "Rolling in " + timeleft;
       timeleft -= 1;
       tickSound.play();
 
-      if(timeleft < 0){
+      if (timeleft < 0) {
         clearInterval(downloadTimer);
-        document.querySelector(".hidden").style.visibility = "visible";
+        document.querySelector(".timer").innerHTML = "Rolling...";
         var allDices = document.querySelectorAll("img");
         var rollDiceSound = new Audio("sounds/RollDice2.mp3");
         rollDiceSound.play();
 
-        for (var i = 0; i < allDices.length; i++){
+        for (var i = 0; i < allDices.length; i++) {
           document.querySelectorAll("img")[i].style.filter = "blur(5px)";
           var rollGif = "gifs/DiceRoll" + Math.floor((Math.random() * 3) + 1) + ".gif";
           document.querySelectorAll("img")[i].setAttribute("src", rollGif);
         }
 
-        setTimeout(function (){
+        setTimeout(function() {
           playGame();
           startTimer();
 
@@ -107,10 +108,9 @@ function startRolling(){
           lastMidBet = parseInt(0);
           lastUpBet = parseInt(0);
           lastDownBet = parseInt(0);
-          for (var i = 0; i < allDices.length; i++){
+          for (var i = 0; i < allDices.length; i++) {
             document.querySelectorAll("img")[i].style.filter = "none";
           }
-          document.querySelector(".hidden").style.visibility = "hidden";
         }, 3000);
 
       }
@@ -118,83 +118,80 @@ function startRolling(){
   }
 }
 
-  function checkIfWon(){
-    var balanceText = document.querySelector("#balance").innerHTML;
-    balanceText = balanceText.match(/\d+/g).map(Number);
-    var balance = parseInt(balanceText, 10);
+function checkIfWon() {
+  var balanceText = document.querySelector("#balance").innerHTML;
+  balanceText = balanceText.match(/\d+/g).map(Number);
+  var balance = parseInt(balanceText, 10);
 
-    var betText = document.querySelector(".betAmount").innerHTML;
-    betText = betText.match(/\d+/g).map(Number);
-    var betAmount = parseInt(betText, 10);
+  var betText = document.querySelector(".betAmount").innerHTML;
+  betText = betText.match(/\d+/g).map(Number);
+  var betAmount = parseInt(betText, 10);
 
-    var betTextDown = document.querySelector("#downBet").innerHTML;
-    betTextDown = betTextDown.match(/\d+/g).map(Number);
-    var betAmountDown = parseInt(betTextDown, 10);
+  var betTextDown = document.querySelector("#downBet").innerHTML;
+  betTextDown = betTextDown.match(/\d+/g).map(Number);
+  var betAmountDown = parseInt(betTextDown, 10);
 
-    var betTextUp = document.querySelector("#upBet").innerHTML;
-    betTextUp = betTextUp.match(/\d+/g).map(Number);
-    var betAmountUp = parseInt(betTextUp, 10);
+  var betTextUp = document.querySelector("#upBet").innerHTML;
+  betTextUp = betTextUp.match(/\d+/g).map(Number);
+  var betAmountUp = parseInt(betTextUp, 10);
 
-    var betTextMid = document.querySelector("#middleBet").innerHTML;
-    betTextMid = betTextMid.match(/\d+/g).map(Number);
-    var betAmountMid = parseInt(betTextMid, 10);
+  var betTextMid = document.querySelector("#middleBet").innerHTML;
+  betTextMid = betTextMid.match(/\d+/g).map(Number);
+  var betAmountMid = parseInt(betTextMid, 10);
 
-    var addBalance;
+  var addBalance;
 
-    if (downWon == true){
-      addBalance = balance + (betAmountDown * 2);
-      document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
-    }
-    else if (upWon == true){
-      addBalance = balance + (betAmountUp * 2);
-      document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
-    }
-    else if (middleWon == true){
-      addBalance = balance + (betAmountMid * 5);
-      document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
-    }
-    else if (up == false && down == false && middle == false){
-      addBalance = balance + betAmount;
-      document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
-    }else {
-      document.querySelector("#balance").innerHTML = "Balance: " + balance;
-    }
-
-    document.querySelector(".betAmount").innerHTML = "Total Bet Amount: 0";
-    document.querySelector("#middleBet").innerHTML = "0";
-    document.querySelector("#upBet").innerHTML = "0";
-    document.querySelector("#downBet").innerHTML = "0";
-
-    down = false;
-    middle = false;
-    up = false;
-    lastBet = 0;
-
+  if (downWon == true) {
+    addBalance = balance + (betAmountDown * 2);
+    document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
+  } else if (upWon == true) {
+    addBalance = balance + (betAmountUp * 2);
+    document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
+  } else if (middleWon == true) {
+    addBalance = balance + (betAmountMid * 5);
+    document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
+  } else if (up == false && down == false && middle == false) {
+    addBalance = balance + betAmount;
+    document.querySelector("#balance").innerHTML = "Balance: " + addBalance;
+  } else {
+    document.querySelector("#balance").innerHTML = "Balance: " + balance;
   }
 
+  document.querySelector(".betAmount").innerHTML = "Total Bet Amount: 0";
+  document.querySelector("#middleBet").innerHTML = "0";
+  document.querySelector("#upBet").innerHTML = "0";
+  document.querySelector("#downBet").innerHTML = "0";
 
-function downFunction(){
+  down = false;
+  middle = false;
+  up = false;
+  lastBet = 0;
+
+}
+
+
+function downFunction() {
   down = true;
   middle = false;
   up = false;
   placeBet();
 }
 
-function middleFunction(){
+function middleFunction() {
   middle = true;
   down = false;
   up = false;
   placeBet();
 }
 
-function upFunction(){
+function upFunction() {
   up = true;
   middle = false;
   down = false;
   placeBet();
 }
 
-document.querySelector("#balance").innerHTML  = "Balance: 1000";
+document.querySelector("#balance").innerHTML = "Balance: 1000";
 
 var bet = parseInt(document.querySelector("#betInput").value);
 var lastBet = parseInt(0);
@@ -206,22 +203,20 @@ var string = document.querySelector("#balance").innerHTML;
 var balance = string.match(/\d+/g).map(Number);
 
 // Check the players bet and remove from balance
-function placeBet(){
+function placeBet() {
   string = document.querySelector("#balance").innerHTML;
   balance = string.match(/\d+/g).map(Number);
   bet = parseInt(document.querySelector("#betInput").value);
 
-  if (balance > bet || balance == bet){
+  if (balance > bet || balance == bet) {
 
-    if (middle == true){
+    if (middle == true) {
       document.querySelector("#middleBet").innerHTML = (bet + lastMidBet);
       lastMidBet = bet + lastMidBet;
-    }
-    else if (up == true){
+    } else if (up == true) {
       document.querySelector("#upBet").innerHTML = (bet + lastUpBet);
       lastUpBet = bet + lastUpBet;
-    }
-    else if (down == true){
+    } else if (down == true) {
       document.querySelector("#downBet").innerHTML = (bet + lastDownBet);
       lastDownBet = bet + lastDownBet;
     }
@@ -230,9 +225,8 @@ function placeBet(){
 
     document.querySelector("#balance").innerHTML = "Balance: " + (balance - bet);
 
-    lastBet = bet +  lastBet;
-  }
-  else if(balance < bet){
+    lastBet = bet + lastBet;
+  } else if (balance < bet) {
     alert("Not Enough Money");
   }
 
